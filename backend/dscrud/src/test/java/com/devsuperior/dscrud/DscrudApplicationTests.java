e007712f022501652363cc0e5a312589d0492ddb
mpsdecamargo <mpsdecamargo@gmail.com>
2021-05-21 13:22:42 -0300

    Project created

---
+++ backend/dscrud/src/test/java/com/devsuperior/dscrud/DscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
