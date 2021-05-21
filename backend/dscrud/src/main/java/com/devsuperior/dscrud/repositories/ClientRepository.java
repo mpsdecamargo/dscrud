23a3760c12d659d924d3caba85aab5ea149c8fe4
mpsdecamargo <mpsdecamargo@gmail.com>
2021-05-21 17:12:47 -0300

    Finished, tested

---
+++ backend/dscrud/src/main/java/com/devsuperior/dscrud/repositories/ClientRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscrud.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dscrud.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
+
+
+
---
+++ backend/dscrud/src/main/java/com/devsuperior/dscrud/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dscrud.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
