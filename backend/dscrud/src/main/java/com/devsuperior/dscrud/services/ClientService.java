d9c230f5ebac38ceeb35e61244b5b48b66a26b15
mpsdecamargo <mpsdecamargo@gmail.com>
2021-05-23 19:37:17 -0300

    Corrected update service exception handling

--- backend/dscrud/src/main/java/com/devsuperior/dscrud/services/ClientService.java
+++ backend/dscrud/src/main/java/com/devsuperior/dscrud/services/ClientService.java
@@ -57,7 +57,7 @@ public class ClientService {
 			return new ClientDTO(entity);
 		}
 		catch (EntityNotFoundException e) {
-			throw new EntityNotFoundException("Id not found" + id);
+			throw new ResourceNotFoundException("Id not found" + id);
 		}
 		
 	}
